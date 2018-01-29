066a753de9975eeae6a0afd7d9b3c482adcae0ae
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-29 12:14:55 +0100

    setup matches index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+admin1 = User.create(email:"test01@test.com",password:"test01",name:"henk",status:"admin")
+admin2 = User.create(email:"test02@test.com",password:"test02",name:"sjaak",status:"admin")
+student1 = User.create(email:"test03@test.com",password:"test03",name:"piet",status:"student")
+student2 = User.create(email:"test04@test.com",password:"test04",name:"karel",status:"student")
+student3 = User.create(email:"test05@test.com",password:"test05",name:"anne",status:"student")
+student4 = User.create(email:"test06@test.com",password:"test06",name:"karin",status:"student")
+student5 = User.create(email:"test07@test.com",password:"test07",name:"jan",status:"student")
+student6 = User.create(email:"test08@test.com",password:"test08",name:"janne",status:"student")
+student7 = User.create(email:"test09@test.com",password:"test09",name:"sanne",status:"student")
+student8 = User.create(email:"test10@test.com",password:"test10",name:"hans",status:"student")
+student9 = User.create(email:"test11@test.com",password:"test11",name:"ans",status:"student")
+student10 = User.create(email:"test12@test.com",password:"test12",name:"frans",status:"student")
+
+Match.create!( [
+  { day:Time.now.strftime("%Y-%m-%d") ,match:student2.name ,user:student1},
+  { day:Time.now.strftime("%Y-%m-%d") ,match:student4.name ,user:student3},
+  { day:Time.now.strftime("%Y-%m-%d") ,match:student6.name ,user:student5},
+  { day:Time.now.strftime("%Y-%m-%d") ,match:student7.name ,user:student8},
+  { day:Time.now.strftime("%Y-%m-%d") ,match:student9.name ,user:student10},
+  ])
