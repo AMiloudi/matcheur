cf427f2224aef0b9ad4d93bac9a11f92a64900c3
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-29 13:57:05 +0100

    added show changed index

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,12 +1,22 @@
 class MatchesController < ApplicationController
   before_action :authenticate_user!
   def index
-    if current_user.status = "admin"
-      @matches = Match.where(day:Time.now.strftime("%Y-%m-%d"))
+    if current_user.status == "admin"
       render :_indexadmin
-    elsif current_user.status = "student"
-      @matches = current_user.matches.where(day:Time.now.strftime("%Y-%m-%d"))
+    elsif current_user.status == "student"
+      @matches = current_user.matches.all
       render :_indexstudent
+    else
+      new_user_session
+    end
+  end
+  def show
+    if current_user.status == "admin"
+      @matches = Match.where(day:Time.now.strftime("%Y-%m-%d"))
+      render :_showadmin
+    elsif current_user.status == "student"
+      @match = current_user.matches.find_by(day:Time.now.strftime("%Y-%m-%d"))
+      render :_showstudent
     else
       new_user_session
     end
