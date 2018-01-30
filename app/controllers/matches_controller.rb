7cc1105e5a78b51a8338b21a39822fc7e45464d3
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-30 17:09:47 +0100

    restful routing except home, navbar links refactored

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,9 +1,19 @@
 class MatchesController < ApplicationController
   before_action :authenticate_user!
-
+  def home
+    if current_user.status == "admin"
+      redirect_to matches_path
+    elsif current_user.status == "student"
+      match = get_student_match_today(current_user)
+      redirect_to match_path(match)
+    else
+      new_user_session
+    end
+  end
   def index
     if current_user.status == "admin"
       @matches = Match.all
+      @matches_today = Match.where(day:Date.today)
       render :_indexadmin
     elsif current_user.status == "student"
       @matches = get_student_matches(current_user)
@@ -43,5 +53,7 @@ class MatchesController < ApplicationController
   def get_student_matches(user)
     Match.where(studenta:user).or(Match.where(studentb:user))
   end
-
+  def get_student_match_today(user)
+    Match.where(studenta:user,day:Date.today).or(Match.where(studentb:user,day:Date.today))
+  end
 end
