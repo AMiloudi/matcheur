6a6212f1c9dfaff5924ac039dde19fc115d28c7d
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-31 10:14:25 +0100

    updated user list with ids, initial js user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,23 @@ class UsersController < ApplicationController
   def show
 
   end
+  def update
+    user = get_user
+    if user.status == "admin"
+      user.status = "student"
+    else
+      user.status = "admin"
+    end
+    render status: 200  json:{status:user.status id:user.id, notice:"status changed"}
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:id)
+  end
+
+  def get_user
+    User.find(user_params)
+  end
 end
