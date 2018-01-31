57abdfe6bf0f32456c6e355808776bd642a9379a
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-31 16:01:29 +0100

    moved around dependencies, no major changes

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,20 +1,20 @@
 class Match < ApplicationRecord
-  belongs_to :studenta, :class_name => 'User'
-  belongs_to :studentb, :class_name => 'User'
+  belongs_to :studenta, class_name:"User"
+  belongs_to :studentb, class_name:"User"
 
   def self.generate_matches(day=Date.today)
-    Match.where(day:day).destroy_all
-    students_array = []
-    students_array = self.get_students
-    uneven(students_array)  #checken if student array is even
-    while students_array.length > 0 do
-      picked_student = User.find(students_array[rand(1...(students_array.length))])
-      first_student = User.find(students_array.first)
+    @day = day
+    Match.where(day:@day).destroy_all
+    @students_array = []
+    @students_array = self.get_students
+    uneven  #checken if student array is even
+    while @students_array != [] do
+      picked_student = User.find(@students_array[rand(1...(@students_array.length))])
+      first_student = User.find(@students_array.first)
       if self.duplicates(picked_student,first_student) == false  #checking wheter a match exists already
-        Match.create(day:day,studenta:picked_student,studentb:first_student)
-        students_array.delete(picked_student.id)
-        students_array.delete(students_array.first) #emptying array from used students
-
+        Match.create(day:@day,studenta:picked_student,studentb:first_student)
+        @students_array.delete(picked_student.id)
+        @students_array.delete(@students_array.first) #emptying array from used students
       end
     end
   end
@@ -40,13 +40,13 @@ class Match < ApplicationRecord
     return dupe
   end
 
-  def self.uneven(students_array)
+  def self.uneven
     dummy = User.find_by(name:"dummy")
-    if students_array.length%2 == 1 && dummy
-      Match.where(studenta_id:dummy).or(Match.where(studentb_id:dummy)).destroy_all
+    if @students_array.length%2 == 1 && User.find_by(name:"dummy")
+      Match.where(studenta:dummy).or(Match.where(studentb:dummy)).destroy_all
       dummy.destroy
       generate_matches
-    elsif students_array.length%2 == 1
+    elsif @students_array.length%2 == 1
       User.create(name:"dummy",password:"dummy1",email:"dummy@test.com",status:"student")
       generate_matches
     end
@@ -57,6 +57,7 @@ class Match < ApplicationRecord
   end
 
   def self.get_students
+
     return User.where(status:"student").all.ids
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-  # has_many :matches, primary_key: :studenta, dependent: :destroy
-  # has_many :matches, primary_key: :studentb, dependent: :destroy
+  # has_many :matches, dependent: :destroy, foreign_key: :studentb
+  # has_many :matches, foreign_key: :studenta, dependent: :destroy
 
 end
